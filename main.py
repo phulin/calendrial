3865e7a1ecffca0089595843f67a95d00897e378
Patrick Hulin <patrick.hulin@gmail.com>
2012-03-23 15:44:09 -0400

    began to write real code, made model

--- main.py
+++ main.py
@@ -1,46 +1,128 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""calendrial, an application for better scheduling.
+"""
+
+__author__ = 'patrick.hulin@gmail.com (Patrick Hulin)'
+
 import settings
 
-import gflags
 import httplib2
+import logging
+import os
+import pickle
+import operator
+from datetime import datetime
+import iso8601
 
 from apiclient.discovery import build
-from oauth2client.file import Storage
-from oauth2client.client import OAuth2WebServerFlow
-from oauth2client.tools import run
+from oauth2client.appengine import oauth2decorator_from_clientsecrets
+from oauth2client.client import AccessTokenRefreshError
+from google.appengine.api import memcache
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
 
-FLAGS = gflags.FLAGS
+# CLIENT_SECRETS, name of a file containing the OAuth 2.0 information for this
+# application, including client_id and client_secret, which are found
+# on the API Access tab on the Google APIs
+# Console <http://code.google.com/apis/console>
+CLIENT_SECRETS = os.path.join(os.path.dirname(__file__), 'client_secrets.json')
 
-# Set up a Flow object to be used if we need to authenticate. This
-# sample uses OAuth 2.0, and we set up the OAuth2WebServerFlow with
-# the information it needs to authenticate. Note that it is called
-# the Web Server Flow, but it can also handle the flow for native
-# applications
-# The client_id and client_secret are copied from the API Access tab on
-# the Google APIs Console
-FLOW = OAuth2WebServerFlow(
-            client_id=settings.client_id,
-            client_secret=settings.client_secret,
-            scope='https://www.googleapis.com/auth/calendar',
-            user_agent='YOUR_APPLICATION_NAME/YOUR_APPLICATION_VERSION')
+# Helpful message to display in the browser if the CLIENT_SECRETS file
+# is missing.
+MISSING_CLIENT_SECRETS_MESSAGE = """
+<h1>Warning: Please configure OAuth 2.0</h1>
+<p>
+To make this sample run you will need to populate the client_secrets.json file
+found at:
+    </p>
+<p>
+<code>%s</code>.
+</p>
+<p>with information found on the <a
+href="https://code.google.com/apis/console">APIs Console</a>.
+</p>
+""" % CLIENT_SECRETS
 
-# To disable the local server feature, uncomment the following line:
-# FLAGS.auth_local_webserver = False
-
-# If the Credentials don't exist or are invalid, run through the native client
-# flow. The Storage object will ensure that if successful the good
-# Credentials will get written back to a file.
-storage = Storage('calendar.dat')
-credentials = storage.get()
-if credentials is None or credentials.invalid == True:
-      credentials = run(FLOW, storage)
-
-# Create an httplib2.Http object to handle our HTTP requests and authorize it
-# with our good Credentials.
-http = httplib2.Http()
-http = credentials.authorize(http)
-
-# Build a service object for interacting with the API. Visit
-# the Google APIs Console
-# to get a developerKey for your own application.
+http = httplib2.Http(memcache)
 service = build(serviceName='calendar', version='v3', http=http,
-               developerKey='YOUR_DEVELOPER_KEY')
+                developerKey=settings.developer_key)
+decorator = oauth2decorator_from_clientsecrets(
+                CLIENT_SECRETS,
+                'https://www.googleapis.com/auth/calendar.readonly',
+                MISSING_CLIENT_SECRETS_MESSAGE)
+
+genpath = lambda s: os.path.join(os.path.dirname(__file__), s)
+
+class Slice(db.Model):
+    guid = StringProperty(required=True)
+    startDate = DateProperty(required=True)
+    endDate = DateProperty(required=True)
+    startTime = DateProperty(required=True)
+    endTime = DateProperty(required=True)
+
+def getEvents(cid, http):
+    response = service.events().list(
+        calendarId = cid,
+        timeMin = '2012-03-20T00:00:00Z',
+        timeMax = '2012-04-01T00:00:00Z',
+        singleEvents = True,
+    ).execute(http)
+    if 'items' in response:
+        return response['items']
+    else:
+        return []
+
+def restrict(parsed, startTime, endTime):
+    new = list()
+    for e in parsed:
+        if e[0] > startTime or e[1] < endTime:
+            if e[0] < startTime: e[0] = startTime
+            if e[1] > endTime: e[1] = endTime
+            new.append(e)
+    return new
+
+class MainHandler(webapp.RequestHandler):
+    @decorator.oauth_required
+    def post(self):
+
+    @decorator.oauth_required
+    def get(self):
+        http = decorator.http()
+        cals = service.calendarList().list(minAccessRole='writer').execute(http)['items']
+        cal_ids = [cal['id'] for cal in cals]
+        events = reduce(operator.add, [getEvents(c, http) for c in cal_ids])
+        start_end = [(e['start']['dateTime'], e['end']['dateTime']) for e in events]
+        parsed = [map(iso8601.parse_date, e) for e in start_end]
+        self.response.out.write(parsed)
+        variables = {
+        }
+        # self.response.out.write(template.render(genpath('index.html'), variables))
+
+def main():
+    application = webapp.WSGIApplication(
+        [
+         ('/', MainHandler),
+        ],
+        debug=True)
+    run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+      main()
